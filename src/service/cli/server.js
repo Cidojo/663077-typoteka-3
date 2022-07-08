b41815df35b3226e8c6cd92b4f4311d4549dab3d
Cidojo <cidojo@yandex.ru>
2022-07-08 03:10:44 +0300

    4-2 добавляет логгер

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,21 +1,38 @@
 'use strict';
 
 const express = require(`express`);
-const chalk = require(`chalk`);
 
 const {HttpCode, API_PREFIX} = require(`../../constants`);
 const routes = require(`../api`);
+const {getLogger} = require(`../lib/logger`);
 
 const DEFAULT_PORT = 3000;
+
+const logger = getLogger({name: `api`});
 
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
+
 app.use(API_PREFIX, routes);
 
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(`Not found`));
+app.use((req, res) => {
+  res
+    .status(HttpCode.NOT_FOUND)
+    .send(`Not found`);
+  logger.error(`Route not found: ${req.url}`);
+});
+
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occurred on processing request: ${err.message}`);
+});
 
 module.exports = {
   name: `--server`,
@@ -23,10 +40,16 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    console.log(__dirname);
-
-    app.listen(port, () => {
-      console.info(chalk.green(`Ожидаю соединений на ${port}`));
-    });
+    try {
+      app.listen(port, (err) => {
+        if (err) {
+          return logger.error(`An error occurred on server creation: ${err.message}`);
+        }
+        return logger.info(`Listening to connections on ${port}`);
+      });
+    } catch (err) {
+      logger.error(`An error occurred: ${err.message}`);
+      process.exit(1);
+    }
   }
 };
